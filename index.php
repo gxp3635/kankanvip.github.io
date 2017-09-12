fa6d372574b2ea6ad258670b25dd779af6276164
gxp3635 <363513764@qq.com>
2017-09-12 16:21:58 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,116 @@
+﻿<html>
+<head>
+<title>VIP视频解析网-免vip看全网视频</title>
+<meta charset="utf-8">
+<meta name="Description" content="VIP视频解析网-免vip看全网视频">
+<script src="/js/jquery.min.js"></script>
+<script src="/js/bootstrap.min.js"></script>
+<script src="js/js.js"></script>
+<link href="/css/bootstrap.css" rel="stylesheet">
+<link href="css/style.css" rel="stylesheet">
+<style>
+.loading{width:100%;
+         height:100%;
+		 position:fixed;
+		 top:0;
+		 left:0;
+		 z-index:9999;
+		 }
+.pic{background:url('image/load.gif');
+     width:64px;
+	 height:64px;
+	 position:absolute;
+	 top:0;
+	 bottom:0;
+	 left:0;
+	 right:0;
+	 margin:auto
+	 }
+
+</style>
+
+</head>
+<body>
+<div class="navbar navbar-default" role="navigation">
+  <div class="navbar-header">
+       <button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".exo">
+         <span class="sr-only">Toggle Navigation</span>
+         <span class="icon-bar"></span>
+         <span class="icon-bar"></span>
+         <span class="icon-bar"></span>
+       </button>
+       <a href="/" class="navbar-brand">VIP视频解析网</a>
+  </div>
+  <div class="collapse navbar-collapse exo">
+    	<ul class="nav navbar-nav">
+      		<li class="active"><a href="#">网站首页</a></li>
+      		<li><a href="api.html">帮助</a></li>
+                <li><h2>提示：有时候播放不了，就拖到底部换个线路</h2></li>
+	 	</ul>
+  </div>
+</div>
+<div class="loading">
+<div class="pic"></div>
+</div>
+<div class="domain">
+<div class="input-group">
+<span class="input-group-addon">电影网址:</span><input style="background: rgba(255, 251, 251, 0.7)" class="form-control" name="url" id="url" placeholder="输入要解析的网址">
+<br />
+</div>
+<button class="btn btn-primary" style="margin-top:1%;margin-bottom:1%;width:100%;font-size:16px" onclick="mr()">播放</button>
+</div>
+
+(此为播放区域，输入网址便会显示)<br />
+<button class="close" data-dismiss="alert">&times;</button>
+</div>
+</div>
+<iframe src="" width="100%" frameborder="0" id="player"></iframe>
+
+
+
+<form class="form-inline">
+<div class="panel panel-info text-center seach">
+<div class="panel panel-heading">
+<span style="font-size:16px">影片搜索</span>
+</div>
+<input type="text" class="form-control" placeholder="输入要搜索的影片名" id="so">
+<button class="btn btn-success" style="margin-bottom:1%"onclick="iqy()">爱奇艺搜索</button>
+<button class="btn btn-primary" style="margin-bottom:1%" onclick="tx()">腾讯视频搜索</button>
+<button class="btn btn-primary" style="margin-bottom:1%" onclick="yk()">优酷视频搜索</button>
+<div class="tip"  style="font-size:16px;text-left;color:red">
+说明:<br />
+1、输入影片名称后会自动跳转到相应网页<br />
+2、直接打开要看的视频<br >
+3、把链接粘贴到上方"电影网址:"那里<br />
+4、即可免费播放<br />
+(app暂不支持)
+</div>
+</div>
+</form>
+
+<div class="panel panel-success player">
+<div class="panel panel-heading text-center">
+<span class="logotext">VIP视频解析网</span>
+</div>
+<div class="btn-list text-center" style="margin-bottom:20px;">
+<button class="btn btn-primary" onclick="api1()">线路一</button>
+<button class="btn btn-success" onclick="api2()">线路二</button>
+<button class="btn btn-info" onclick="api3()">线路三</button>
+<button class="btn btn-warning" onclick="api4()">线路四</button>
+<button class="btn btn-danger" onclick="api5()">线路五</button>
+<br /><br />
+<div class="alert alert-danger" role="alert">温馨提示:有时候播放不了就换个线路噢！<br />
+
+
+<p class="text-center" style="font-size:17px"><br>&copy; Powered by:剧终一曲流年 2017 版权所有
+<script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1263660986'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s19.cnzz.com/z_stat.php%3Fid%3D1263660986%26show%3Dpic1' type='text/javascript'%3E%3C/script%3E"));</script>
+</p>
+<h6 class="text-center" style="font-size:15px;">友情链接:
+<a href="http://  target="_blank">无</a>
+<a href="http://  target="_blank">无</a>
+
+</h6>
+</div>
+</div>
+</body>
+</html>
